1badf5b1aac663de19561e7ed998fe35d0e47254
Deivid Carvalho <deivid.carvalho.cruz@gmail.com>
2023-04-23 16:06:55 -0300

    TELA DA CALCULADORA FINALIZADA
    FINALIZADO A TELA!

--- src/br/com/cod3r/calc/visao/Teclado.java
+++ src/br/com/cod3r/calc/visao/Teclado.java
@@ -1,43 +1,76 @@
 package br.com.cod3r.calc.visao;
 
 import java.awt.Color;
-import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Teclado extends JPanel {
 
+	//Cores dos botões
 	private final Color COR_CINZA_ESCURO = new Color(68, 68, 68);
 	private final Color COR_CINZA_CLARO = new Color(99, 99, 99);
 	private final Color COR_LARANJA = new Color(242, 163, 60);
 
 	public Teclado() {
-		setLayout(new GridLayout(5, 4));
-		add(new Botao("AC", COR_CINZA_ESCURO));
-		add(new Botao("+-", COR_CINZA_ESCURO));
-		add(new Botao("%", COR_CINZA_ESCURO));
-		add(new Botao("/", COR_LARANJA));
 
-		add(new Botao("7", COR_CINZA_CLARO));
-		add(new Botao("8", COR_CINZA_CLARO));
-		add(new Botao("9", COR_CINZA_CLARO));
-		add(new Botao("*", COR_LARANJA));
+		GridBagLayout layout = new GridBagLayout();
+		GridBagConstraints c = new GridBagConstraints();
 
-		add(new Botao("4", COR_CINZA_CLARO));
-		add(new Botao("5", COR_CINZA_CLARO));
-		add(new Botao("6", COR_CINZA_CLARO));
-		add(new Botao("-", COR_LARANJA));
+		setLayout(layout); //Setamos o Layout passando o GridBadLayout
+		
+		c.fill =  GridBagConstraints.BOTH; //Preencher os espaços
+		c.weightx = 1; //peso eixo X (horizontal)
+		c.weighty = 1; //peso eixo Y (vertical)
 
-		add(new Botao("1", COR_CINZA_CLARO));
-		add(new Botao("2", COR_CINZA_CLARO));
-		add(new Botao("3", COR_CINZA_CLARO));
-		add(new Botao("+", COR_LARANJA));
+		// LINHA 1
+		c.gridwidth = 3; //peso eixo X (horizontal) -> Botão abaixo vai ocupar espaço de 3
+		adicionarBotao("AC", COR_CINZA_ESCURO, c, 0, 0);
+		
+		//adicionarBotao("+/-", COR_CINZA_ESCURO, c, 1, 0);
+		//adicionarBotao("%", COR_CINZA_ESCURO, c, 2, 0);
+		
+		c.gridwidth = 1; //peso eixo X (horizontal) -> Botão abaixo vai ocupar espaço de 1
+		adicionarBotao("/", COR_LARANJA, c, 3, 0);
 
-		add(new Botao("0", COR_CINZA_CLARO));
-		add(new Botao(",", COR_CINZA_CLARO));
-		add(new Botao("=", COR_CINZA_CLARO));
-		add(new Botao("<", COR_LARANJA));
+		// LINHA 2
+		adicionarBotao("7", COR_CINZA_CLARO, c, 0, 1);
+		adicionarBotao("8", COR_CINZA_CLARO, c, 1, 1);
+		adicionarBotao("9", COR_CINZA_CLARO, c, 2, 1);
+		adicionarBotao("*", COR_LARANJA, c, 3, 1);
+		
+		// LINHA 3
+		adicionarBotao("4", COR_CINZA_CLARO, c, 0, 2);
+		adicionarBotao("5", COR_CINZA_CLARO, c, 1, 2);
+		adicionarBotao("6", COR_CINZA_CLARO, c, 2, 2);
+		adicionarBotao("-", COR_LARANJA, c, 3, 2);
+		
+		// LINHA 4
+		adicionarBotao("1", COR_CINZA_CLARO, c, 0, 3);
+		adicionarBotao("2", COR_CINZA_CLARO, c, 1, 3);
+		adicionarBotao("3", COR_CINZA_CLARO, c, 2, 3);
+		adicionarBotao("+", COR_LARANJA, c, 3, 3);
+		
+		// LINHA 5
+		c.gridwidth = 2;
+		adicionarBotao("0", COR_CINZA_CLARO, c, 0, 4);
+		
+		c.gridwidth = 1;
+		adicionarBotao(",", COR_CINZA_CLARO, c, 2, 4);
+		adicionarBotao("=", COR_LARANJA, c, 3, 4);
+
+	}
+
+
+	//Método para criar os botões no GridBagLayout
+	private void adicionarBotao(String texto, Color cor, GridBagConstraints c, int x, int y) {
+		c.gridx = x;
+		c.gridy = y;
+		Botao botao = new Botao(texto, cor);
+		add(botao, c);
+
 	}
 
 }
